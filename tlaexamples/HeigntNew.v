a2301da95022cddca5650d7ed74629b33ff87006
Daniel Ricketts <daniel.bmore.ricketts@gmail.com>
2014-11-05 03:57:20 +0100

    Progress on height controller with independent read and control.

--- tlaexamples/HeigntNew.v
+++ tlaexamples/HeigntNew.v
@@ -6,106 +6,96 @@ Require Import TLA.ProofRules.
 Require Import TLA.Tactics.
 Require Import Coq.Reals.Rdefinitions.
 Require Import Coq.Reals.RIneq.
+
 Open Scope HP_scope.
 Open Scope string_scope.
+
 Section HeightCtrl.
-Variable d : R.
-(* The following hypothesis is not necessary
-for the safety property, but it's necessary
-for ensuring that non-Zeno behaviors are
-possible. *)
-Hypothesis Hd : (d > 0)%R.
-Definition Read : Formula :=
-"H"! = "h".
-Definition Evolve : Formula :=
-Continuous (["h"' ::= "v",
-"v"' ::= 0,
-"t"' ::= 1,
-"H"' ::= 0,
-"H1"' ::=0,
-"TC"' ::= 0,
-"TR"' ::= 0]).
 
-Definition Ctrl : Formula :=
-("H1" < 0 /\ "v"! = 1 /\ "H1"! = "H") 
-\/ ("H1" >= 0 /\ "v"! = --1 /\ "H1"!="H").
+  Variable d : R.
+  (* The following hypothesis is not necessary
+     for the safety property, but it's necessary
+     for ensuring that non-Zeno behaviors are
+     possible. *)
+  Hypothesis Hd : (d > 0)%R.
 
+  Definition Read : Formula :=
+    "H"! = "h".
 
-Definition Next : Formula :=
-(Evolve /\ "t"! <= "TC" + d /\ "t"! <= "TR" + d)
-\/ (Ctrl /\ "TC"! = "t" /\ Unchanged (["h", "t", "H", "TR"]))
-\/ (Read /\ "TR"! = "t" /\ Unchanged (["h", "t", "v", "TC", "H1"])).
+  Definition Evolve : Formula :=
+    Continuous (["h"' ::= "v",
+                 "v"' ::= 0,
+                 "t"' ::= 1,
+                 "H"' ::= 0,
+                 "H1"' ::=0,
+                 "TC"' ::= 0,
+                 "TR"' ::= 0]).
+  
+  Definition Ctrl : Formula :=
+       ("H1" < 0 /\ "v"! = 1 /\ "H1"! = "H") 
+    \/ ("H1" >= 0 /\ "v"! = --1 /\ "H1"!="H").
 
 
+  Definition Next : Formula :=
+       (Evolve /\ "t"! <= "TC" + d /\ "t"! <= "TR" + d)
+    \/ (Ctrl /\ "TC"! = "t" /\ Unchanged (["h", "t", "H", "TR"]))
+    \/ (Read /\ "TR"! = "t" /\ Unchanged (["h", "t", "v", "TC", "H1"])).
 
+  Definition Init : Formula :=
+    (--1 = "v" \/ "v" = 1) /\
+    --d <= "h" <= d /\
+    "t" = "TC" /\ "t" = "TR" /\ "H" = "h" /\ "H1" = "h".
 
-Definition Init : Formula :=
-(--1 = "v" \/ "v" = 1) /\
---d <= "h" <= d /\
-"t" = "TC" /\ "t" = "TR" /\ "H" = "h" /\ "H1" = "h".
+  Definition Safe : Formula :=
+    --3*d <="h" <= 3*d.
 
+  Notation "x <= y < z" :=
+    (And (Comp x y Le) (Comp y z Lt)) : HP_scope.
 
+  Notation "x < y <= z" :=
+    (And (Comp x y Lt) (Comp y z Le)) : HP_scope.
 
-Definition Safe : Formula :=
---3*d <="h" <= 3*d.
+  Definition Ind_Inv : Formula :=
+    ("v" = 1   --> (--3*d <= "H1" <= d /\
+                    0 <= "H"-"H1" <= "TR"-"TC" /\
+                    0 <= "h"-"H" <= "t"-"TR")) /\
+    ("v" = --1 --> (--d <= "H1" <= 3*d /\
+                    0 <= "H1"-"H" <= "TR"-"TC" /\
+                    0 <= "H"-"h" <= "t"-"TR")) /\
+    0 <= "t"-"TR" <= d /\
+    0 <= "t"-"TC" <= d /\
+    --d <= "TR" - "TC" <= d /\
+    ("v"=--1 \/ "v" = 1).
+(*
+    (("v" = 1 /\ "h" < "H1") --> ((--3*d + ("TC"-"TR"+"d")) <= "H1" < 0) /\
+     (--("TC"-"TR"+"d") <= "h"-"H1" < 0 )) /\
+    (("v" = 1 /\ "h" >= "H1") --> (( --3*d <= "H1" <= (3*d - ("TC"-"TR"+2*d))) /\
+                                   ( 0 <="h"-"H1"<= ("TC"-"TR"+ 2*d)))) /\
+    (("v" = --1 /\ "h" > "H1") --> (( 0 < "H1" <= (3*d - ("TC"-"TR"+d))) /\
+                                    (0 <"h"-"H1" <= ("TC" - "TR" +d)))) /\
+    (("v"= --1 /\ "h" <= "H1") --> (( (--3*d + ("TC"-"TR"+2*d) )<= "H1" <= 3*d) /\
+                                    (--("TC"- "TR" + 2*d) <= "h"-"H1" <= 0))) /\
+    ("v"=--1 \/ "v" = 1).
+*)
 
-Notation "x <= y < z" :=
-(And (Comp x y Le) (Comp y z Lt)) : HP_scope.
+  Lemma ind_inv_init : |- Init --> Ind_Inv.
+  Proof. solve_linear. Qed.
 
-Notation "x < y <= z" :=
-(And (Comp x y Lt) (Comp y z Le)) : HP_scope.
+  Lemma ind_inv_safe : |- Ind_Inv --> Safe.
+  Proof. solve_linear. Qed. 
 
-Definition Ind_Inv : Formula :=
-(("v" = 1 /\ "h" < "H1") --> ((--3*d + ("TC"-"TR"+"d")) <= "H1" < 0) /\
-(--("TC"-"TR"+"d") <= "h"-"H1" < 0 )) /\
-(("v" = 1 /\ "h" >= "H1") --> (( --3*d <= "H1" <= (3*d - ("TC"-"TR"+2*d))) /\
-( 0 <="h"-"H1"<= ("TC"-"TR"+ 2*d)))) /\
-(("v" = --1 /\ "h" > "H1") --> (( 0 < "H1" <= (3*d - ("TC"-"TR"+d))) /\
-(0 <"h"-"H1" <= ("TC" - "TR" +d)))) /\
-(("v"= --1 /\ "h" <= "H1") --> (( (--3*d + ("TC"-"TR"+2*d) )<= "H1" <= 3*d) /\
-(--("TC"- "TR" + 2*d) <= "h"-"H1" <= 0))) /\
-("v"=--1 \/ "v" = 1).
+  Lemma safety :
+  |- (Init /\ []Next) --> []Safe.
+  Proof.
+    apply imp_trans with (F2:=Ind_Inv /\ []Next).
+    - apply and_right.
+      + apply and_left1. apply ind_inv_init.
+      + apply and_left2. apply imp_id.
+    - apply imp_trans with (F2:=[]Ind_Inv).
+      + apply inv_discr_ind; auto. unfold Next, Evolve.
+        Time prove_inductive. solve_linear.
+      + apply always_imp. apply ind_inv_safe.
 
-
-
-
-Lemma or_left : 
-forall F1 F2 F3,
-(|- F1 --> F3) ->
-(|- F2 --> F3) ->
-(|- (F1 \/ F2) --> F3).
-Proof. firstorder. Qed.
-
-Lemma or_right1 :
-forall F1 F2 F3,
-(|- F1 --> F2) ->
-(|- F1 --> (F2 \/ F3)).
-Proof. firstorder. Qed.
-
-Lemma or_right2 :
-forall F1 F2 F3,
-(|- F1 --> F3) ->
-(|- F1 --> (F2 \/ F3)).
-Proof. firstorder. Qed.
-
-Lemma and_some:
-forall L1 L2 L3 L4 L5,
-((|- L1 --> L5) /\ (|- L2 --> L5) /\ (|- L3 --> L5) /\ (|- L4 --> L5)) ->
-(|- ((L1 /\ L2 /\ L3 /\ L4) --> L5)).
-Proof. firstorder. Qed. 
-
-Lemma ind_inv_init : |- Init --> Ind_Inv.
-Proof. solve_linear. Qed.
-
-
-
-Lemma ind_inv_safe : |- Ind_Inv --> Safe.
-Proof. solve_linear. Qed. 
-
-
-Lemma safety :
-|- (Init /\ []Next) --> []Safe.
-Proof.
 apply imp_trans with (F2:=Ind_Inv /\ []Next).
 - apply and_right.
 + apply and_left1. apply ind_inv_init.
