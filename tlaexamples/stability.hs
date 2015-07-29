a9728bd0ae26dfa73029b19911f2a221a710f0bf
Matthew Chan <matt@themattchan.com>
2015-07-29 01:10:40 -0700

    graphs work on racket

--- tlaexamples/stability.hs
+++ tlaexamples/stability.hs
@@ -7,21 +7,22 @@ import Graphics.EasyPlot
 velocity = -1
 epsilon = 0.25
 
-initial = \x -> (-100 *x) + 100
+initial = \x -> (-1 *x) + 1
 
 nextF f t = \x -> ((-1) * (f t) * (x - t)) + (f t)
 
 times = iterate (+ epsilon) 0
 
-functions = foldl (\a@(x:xs) t -> nextF x t : a) [initial] times
+functions = foldl (\a@(f:fs) t -> nextF f t : a) [initial] times
 
 --randomInterrupts :: [Double]
 --randomInterrupts = runState (
 
 graphs :: [Graph2D Double Double]
-graphs = map (Function2D [] []) $ take 10 functions
+graphs = map (Function2D [Title "STRING"] [Range 0 20]) $
+  take 10 functions
 
 main :: IO ()
 main = do
-   plot (PNG "plot.png")  graphs
+   plot X11 graphs
    return ()
---
+++ tlaexamples/stability.rkt
@@ -0,0 +1,50 @@
+#lang racket
+
+(require plot)
+
+(define velocity -1)
+
+(define epsilon 0.5)
+
+(define initial
+  (λ (x) (add1 (* -1 x))))
+
+(define (next-function f t) 
+  (λ (x) (+ (* -1 (f t) (- x t)) (f t))))
+ 
+(define constant-interrupts
+  (letrec ([go
+            (lambda (a n)
+              (if (= n 0)
+                  a
+                  (go (cons (+ epsilon (car a)) a)
+                        (sub1 n))))])
+    (reverse (go '(0) 10))))
+                
+
+(define (scan f init xs)
+  (if (null? xs)
+      '()
+      (let ((v (f init (car xs))))
+        (cons v
+         (scan f v (cdr xs)))))) 
+        
+(define (random-interrupts)
+  (let [(randoms 
+         (let [(gen (make-pseudo-random-generator))]
+           (for/list [(i 10)]
+             (random gen))))]
+    (scan + 0 (cons 0 randoms))))
+
+(define (functions)
+  (foldl (λ (x a) 
+           (cons (next-function (car a) x)
+                 a))
+         (list initial)
+         (random-interrupts)))
+
+(define (graph)
+  (plot (map (λ (f) (function f 0 5)) (functions))
+        #:x-min 0 #:y-min 0
+        #:x-label "time"	 
+        #:y-label "x"))
